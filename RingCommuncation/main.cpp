7bb3ca1b20ca2e788d545700aee2bf4405f3fddb
allywarner <allywarner@sci.utah.edu>
2016-03-28 21:55:20 -0600

    Comments and build script

--- RingCommuncation/main.cpp
+++ RingCommuncation/main.cpp
@@ -13,41 +13,46 @@
 
 using namespace std;
 
-
 //MAIN FUNCTION
+//Inputs: the numebr of times you'd like to go around the ring
 int main(int argc, char* argv[]){
     
+    //Initialize
     MPI_Init(&argc,&argv);
     
+    //Throws an error if you do not input a number of times you'd like to go around the ring.
     if (argc < 2){
         cerr << "Error. Please input how many times you'd like the number of times you'd like the number to go around the ring.";
         return 1;
     }
     
+    //Number of times you'd like to go around the ring
     int numAroundRing = atoi(argv[1]);
     
+    //Getting the rank and the size
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     
+    //Initialize the integer to be passed around the ring as zero
     int number = 0;
     
+    //Start the timing
     double startTime = MPI_Wtime();
-    
+    //Going around the ring as many times as specified
     for (int i = 0; i < numAroundRing; i++) {
-        
+        //checking for boundary conditions
         if (world_rank != world_size - 1) {
             MPI_Send(&number, 1, MPI_INT, world_rank + 1, 0, MPI_COMM_WORLD);
         } else if (world_rank == world_size - 1) {
             MPI_Send(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
         }
         
-        
+        //checking for boundary conditions
         if (world_rank != 0) {
             MPI_Recv(&number, 1, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             number += world_rank;
-            //printf("Process %d received number %d from process %d\n", world_rank, number, world_rank - 1);
         } else if (world_rank == 0) {
             MPI_Recv(&number, 1, MPI_INT, world_size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             number += world_rank;
